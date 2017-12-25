e81b42e94393f1b8e99823e074870ffb239a76f8
xuwenjin01 <xuwenjin1212@qq.com>
2017-12-25 14:19:32 +0800

    添加redis和拦截器

---
+++ alo7_inf/src/main/java/cn/com/alo7/inf/common/TokenInterceptor.java
@@ -0,0 +1,44 @@
+package cn.com.alo7.inf.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import cn.com.alo7.inf.service.IRedisService;
+
+@Component
+public class TokenInterceptor implements HandlerInterceptor {
+
+	@Autowired
+	private IRedisService redisService;
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		if(request.getHeader(Constant.TOKEN).equals(redisService.get(Constant.TOKEN))){
+			return true;
+		}
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("postHandle");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ alo7_inf/src/main/java/cn/com/alo7/inf/service/impl/RedisServiceImpl.java
@@ -0,0 +1,87 @@
+package cn.com.alo7.inf.service.impl;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.stereotype.Service;
+
+import cn.com.alo7.inf.service.IRedisService;
+
+/**
+ * redis
+ * 
+ */
+@Service
+public class RedisServiceImpl implements IRedisService
+{
+
+	@Autowired  
+    private RedisTemplate<String, ?> redisTemplate;  
+      
+    @Override  
+    public boolean set(final String key, final String value) {  
+        boolean result = redisTemplate.execute(new RedisCallback<Boolean>() {  
+            @Override  
+            public Boolean doInRedis(RedisConnection connection) throws DataAccessException {  
+                RedisSerializer<String> serializer = redisTemplate.getStringSerializer();  
+                connection.set(serializer.serialize(key), serializer.serialize(value));  
+                return true;  
+            }  
+        });  
+        return result;  
+    }  
+  
+    public String get(final String key){  
+        String result = redisTemplate.execute(new RedisCallback<String>() {  
+            @Override  
+            public String doInRedis(RedisConnection connection) throws DataAccessException {  
+                RedisSerializer<String> serializer = redisTemplate.getStringSerializer();  
+                byte[] value =  connection.get(serializer.serialize(key));  
+                return serializer.deserialize(value);  
+            }  
+        });  
+        return result;  
+    }  
+  
+    @Override  
+    public boolean expire(final String key, long expire) {  
+        return redisTemplate.expire(key, expire, TimeUnit.SECONDS);  
+    }
+
+	@Override
+	public <T> boolean setList(String key, List<T> list) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public <T> List<T> getList(String key, Class<T> clz) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long lpush(String key, Object obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long rpush(String key, Object obj) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String lpop(String key) {
+		// TODO Auto-generated method stub
+		return null;
+	}  
+     
+}

3908a271a66581439bc0d4e88e8a5fcff57d4878
mzanthem-local <mzanthem@gmail.com>
2017-12-30 02:13:45 +0800

    typeWithPage

--- alo7_inf/src/main/java/cn/com/alo7/inf/repository/AlbumFullViewRepository.java
+++ alo7_inf/src/main/java/cn/com/alo7/inf/repository/AlbumFullViewRepository.java
@@ -1,7 +1,5 @@
 package cn.com.alo7.inf.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,14 +14,6 @@ import cn.com.alo7.inf.entity.AlbumFullView;
 public interface AlbumFullViewRepository extends JpaRepository<AlbumFullView, Long>{
 	
 	
-	/**
-	 * 专辑翻页查询
-	 * @param pageable
-	 * @param type
-	 * @return
-	 */
-	Page<AlbumFullView> findByTypeWithPage(Pageable pageable, String type);
-
 	/**
 	 * 根据类型和专辑code查找特殊专辑信息
 	 * @param albumTypeSpecial
